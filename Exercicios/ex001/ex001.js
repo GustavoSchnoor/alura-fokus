64c420f41c6e96d53f852fff600bf76551a08436
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-15 17:27:57 -0300

    att ex001

--- Exercicios/ex001/ex001.js
+++ Exercicios/ex001/ex001.js
@@ -12,5 +12,36 @@ const duracaoFoco = 1500;
 const duracaoDescansoCurto = 300;
 const duracaoDescansoLongo = 900;
 
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break;
+    }
+}
+
+focoBtn.addEventListener('click', function() {
+    alterarContexto('foco');
+})
+
+curtoBtn.addEventListener('click', function() {
+    alterarContexto('descanso-curto');
+})
+
+longoBtn.addEventListener('click', function() {
+    alterarContexto('descanso-longo');
+})
+
+
 
 
