36a4a23a577646bd7bc4be6cdb938b8ecef12952
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-04-15 13:02:55 -0300

    att ex001

---
+++ Exercicios/ex001/ex001.js
@@ -0,0 +1,16 @@
+const html = document.querySelector('html');
+const displayTempo = document.querySelector('.app__card-timer');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+
+const botaoComecar = document.querySelector('.app__card-primary-button');
+const focoBtn = document.querySelector('.app__card-button--foco');
+const curtoBtn = document.querySelector('.app__card-button--curto');
+const longoBtn = document.querySelector('.app__card-button--longo');
+
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
+
+
